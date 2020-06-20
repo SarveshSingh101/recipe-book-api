ef40f31347d73919f9df7728d4011c9823a07bb7
sarvesh singh <sarveshsingh101@gmail.com>
2020-06-20 06:00:54 +0530

    Initial Commit

---
+++ recipe-book-domain/Models/Recipe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RecipeBook.Domain.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }   
+        public DateTime CreatedAt { get; set; }
+        public DateTime CreatedBy { get; set; }
+    }
+}
